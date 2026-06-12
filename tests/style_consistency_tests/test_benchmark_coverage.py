5c36300f36c8a97b772eb4bf2609d5e5e1f8b72a
Ben Viggiano <viggiano.benjamin@gmail.com>
2026-06-12 14:54:12 -0700

    Update

--- tests/style_consistency_tests/test_benchmark_coverage.py
+++ tests/style_consistency_tests/test_benchmark_coverage.py
@@ -38,7 +38,6 @@ _EXEMPT_TOOLS = frozenset(
 # instead. The test below keeps this set honest.
 _KNOWN_MISSING = frozenset(
     {
-        "malinois-gradient",
         "malinois-score",
         "meme-fimo-scan",
         "miranda-scan",
